ffbcfceebedcc1fefe5760b2709eb75bc56b2dad
Jozell <jozellrili@gmail.com>
2019-12-15 01:14:05 +0800

    updated the changed password UI

--- resources/views/auth/change_password.blade.php
+++ resources/views/auth/change_password.blade.php
@@ -17,57 +17,43 @@
         @else
         {!! Form::open(['method' => 'PATCH', 'route' => ['auth.change_password']]) !!}
         <!-- If no success message in flash session show change password form  -->
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                @lang('quickadmin.qa_edit')
-            </div>
 
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-xs-12 form-group">
-                        {!! Form::label('current_password', trans('quickadmin.qa_current_password'), ['class' =>
-                        'control-label']) !!}
-                        {!! Form::password('current_password', ['class' => 'form-control', 'placeholder' => '']) !!}
-                        <p class="help-block"></p>
-                        @if($errors->has('current_password'))
-                        <p class="help-block">
-                            {{ $errors->first('current_password') }}
-                        </p>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-12 form-group">
-                        {!! Form::label('new_password', trans('quickadmin.qa_new_password'), ['class' =>
-                        'control-label']) !!}
-                        {!! Form::password('new_password', ['class' => 'form-control', 'placeholder' => '']) !!}
-                        <p class="help-block"></p>
-                        @if($errors->has('new_password'))
-                        <p class="help-block">
-                            {{ $errors->first('new_password') }}
-                        </p>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-12 form-group">
-                        {!! Form::label('new_password_confirmation', trans('quickadmin.qa_password_confirm'), ['class'
-                        => 'control-label']) !!}
-                        {!! Form::password('new_password_confirmation', ['class' => 'form-control', 'placeholder' =>
-                        '']) !!}
-                        <p class="help-block"></p>
-                        @if($errors->has('new_password_confirmation'))
-                        <p class="help-block">
-                            {{ $errors->first('new_password_confirmation') }}
-                        </p>
-                        @endif
-                    </div>
-                </div>
-            </div>
+        <div class="position-relative form-group">
+            {!! Form::label('current_password', trans('quickadmin.qa_current_password'), ['class' =>
+            'control-label']) !!}
+            {!! Form::password('current_password', ['class' => 'form-control form-control-sm', 'placeholder' => '']) !!}
+            @if($errors->has('current_password'))
+            <p class="help-block text-danger">
+                {{ $errors->first('current_password') }}
+            </p>
+            @endif
         </div>
-        {!! Form::submit(trans('quickadmin.qa_save'), ['class' => 'btn btn-danger']) !!}
+
+        <div class="position-relative form-group">
+            {!! Form::label('new_password', trans('quickadmin.qa_new_password'), ['class' =>
+            'control-label']) !!}
+            {!! Form::password('new_password', ['class' => 'form-control form-control-sm', 'placeholder' => '']) !!}
+            @if($errors->has('new_password'))
+            <p class="help-block text-danger">
+                {{ $errors->first('new_password') }}
+            </p>
+            @endif
+        </div>
+
+        <div class="position-relative form-group">
+            {!! Form::label('new_password_confirmation', trans('quickadmin.qa_password_confirm'), ['class'
+            => 'control-label']) !!}
+            {!! Form::password('new_password_confirmation', ['class' => 'form-control form-control-sm', 'placeholder' =>
+            '']) !!}
+            @if($errors->has('new_password_confirmation'))
+            <p class="help-block text-danger">
+                {{ $errors->first('new_password_confirmation') }}
+            </p>
+            @endif
+        </div>
+        <div class="position-relative">
+            {!! Form::submit(trans('quickadmin.qa_save'), ['class' => 'btn btn-primary btn-sm']) !!}
+        </div>
         {!! Form::close() !!}
         @endif
     </div>
